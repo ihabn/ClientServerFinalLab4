bb7d0f2f958859344e909df6b8e51e253deffc0d
ihabn <ayziguy@gmail.com>
2022-10-05 08:36:20 +0500

    Initial commit

---
+++ src/server/ClientHandler.java
@@ -0,0 +1,71 @@
+package server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ClientHandler implements Runnable {
+
+    private Server server;
+    private PrintWriter outMessage;
+    private Scanner inMessage;
+    private static final String HOST = "localhost";
+    private static final int PORT = 3443;
+    private Socket clientSocket = null;
+    private static int clients_count = 0;
+
+    public ClientHandler(Socket socket, Server server) {
+        try {
+            clients_count++;
+            this.server = server;
+            this.clientSocket = socket;
+            this.outMessage = new PrintWriter(socket.getOutputStream());
+            this.inMessage = new Scanner(socket.getInputStream());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                server.sendMessageToAllClients("New user in the chat!");
+                server.sendMessageToAllClients("Clients count = " + clients_count);
+                break;
+            }
+
+            while (true) {
+                if (inMessage.hasNext()) {
+                    String clientMessage = inMessage.nextLine();
+                    if (clientMessage.equalsIgnoreCase("##session##end##")) {
+                        break;
+                    }
+                    System.out.println(clientMessage);
+                    server.sendMessageToAllClients("Client: " + clientMessage);
+                }
+                Thread.sleep(100);
+            }
+        }
+        catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            this.close();
+        }
+    }
+    public void sendMsg(String msg) {
+        try {
+            outMessage.println(msg);
+            outMessage.flush();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    public void close() {
+        server.removeClient(this);
+        clients_count--;
+        server.sendMessageToAllClients("Clients count = " + clients_count);
+    }
+}
